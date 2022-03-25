3dd303e1111403331d8db1c804ebdc89e91d2f58
LukaFlores <luka.flores@mail.com>
2022-03-25 03:12:30 -0700

    feature(ticketing): Auth signout & CurrentUser Completed

--- ticketing/auth/src/routes/current-user.ts
+++ ticketing/auth/src/routes/current-user.ts
@@ -1,9 +1,10 @@
 import express from 'express';
+import { curentUser } from '../middlewares/current-user';
 
 const router = express.Router();
 
-router.get('/api/users/currentuser', (req, res) => {
-  res.send('hi there');
+router.get('/api/users/currentuser', curentUser, (req, res) => {
+  res.send({ currentUser: req.currentUser || null });
 });
 
 export { router as currentUserRouter };
