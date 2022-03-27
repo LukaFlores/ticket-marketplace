740b75d565c2ab9f16ed236a9ef24ca84602c9a2
LukaFlores <luka.flores@mail.com>
2022-03-26 19:11:31 -0700

    Common Publish Updates

--- ticketing/tickets/src/app.ts
+++ ticketing/tickets/src/app.ts
@@ -6,6 +6,7 @@ import cookieSession from 'cookie-session';
 
 import { errorHandler, NotFoundError, curentUser } from '@lukaflorestickets/common';
 import { createTicketRouter } from './routes/new';
+import { showTicketRouter } from './routes/show';
 
 const app = express();
 
@@ -21,6 +22,9 @@ app.use(
 app.use(curentUser);
 
 app.use(createTicketRouter);
+app.use(showTicketRouter);
+
+
 
 app.all('*', async () => {
   throw new NotFoundError();
