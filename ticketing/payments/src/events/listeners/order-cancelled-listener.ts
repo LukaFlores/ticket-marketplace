455dfba5d586fdbcae19bea5c10ed9681de10795
LukaFlores <luka.flores@mail.com>
2022-04-01 14:30:25 -0700

    feature(ticketing): add listeners for order cancelled payments service

---
+++ ticketing/payments/src/events/listeners/order-cancelled-listener.ts
@@ -0,0 +1,24 @@
+import { OrderCancelledEvent, OrderStatus, Subjects } from '@lukaflorestickets/common';
+import Listener from '@lukaflorestickets/common/build/events/base/base-listener';
+import { Message } from 'node-nats-streaming';
+import { Order } from '../../models/order';
+import { queueGroupName } from '../queue-group-name';
+
+export class OrderCancelledListener extends Listener<OrderCancelledEvent> {
+  readonly subject = Subjects.OrderCancelled;
+  queueGroupName = queueGroupName;
+  async onMessage(data: OrderCancelledEvent['data'], msg: Message) {
+    const order = await Order.findOne({
+      _id: data.id,
+      version: data.version - 1,
+    });
+
+    if (!order) {
+      throw new Error('Order Not found');
+    }
+    order.set({ status: OrderStatus.Cancelled });
+    await order.save();
+
+    msg.ack();
+  }
+}
